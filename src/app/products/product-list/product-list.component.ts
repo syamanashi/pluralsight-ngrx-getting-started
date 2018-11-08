7f715ac06ce1084d3c5b8184d1457a9298524f73
Sutton Yamanashi <syamanashi@ast.com>
2018-11-08 16:54:34 -0500

    Subscribing to the Store to get State Changes

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -4,7 +4,7 @@ import { Subscription } from 'rxjs';
 
 import { Product } from '../product';
 import { ProductService } from '../product.service';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 
 @Component({
   selector: 'pm-product-list',
@@ -37,6 +37,13 @@ export class ProductListComponent implements OnInit, OnDestroy {
       (products: Product[]) => this.products = products,
       (err: any) => this.errorMessage = err.error
     );
+
+    // TODO: Unsubscribe
+    this.store.pipe(select('products')).subscribe(products => {
+      if (products) {
+        this.displayCode = products.showProductCode;
+      }
+    });
   }
 
   ngOnDestroy(): void {
