95d7f33fb80ece146332ba4de07d9405ffcc05d1
Sutton Yamanashi <syamanashi@ast.com>
2018-11-10 09:42:38 -0500

    Implementing Effects to Load Products

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -37,10 +37,15 @@ export class ProductListComponent implements OnInit, OnDestroy {
       this.selectedProduct = currentProduct;
     });
 
-    this.productService.getProducts().subscribe(
-      (products: Product[]) => this.products = products,
-      (err: any) => this.errorMessage = err.error
-    );
+    // this.productService.getProducts().subscribe(
+    //   (products: Product[]) => this.products = products,
+    //   (err: any) => this.errorMessage = err.error
+    // );
+
+    this.store.dispatch(new productActions.Load());
+    // TODO: Unsubscribe
+    this.store.pipe(select(fromProduct.getProducts))
+      .subscribe((products: Product[]) => this.products = products);
 
     // TODO: Unsubscribe
     this.store.pipe(select(fromProduct.getShowProductCode)).subscribe(showProductCode => {
